810dfc523bfa333f2903df3a540ca4979aacaa0c
NocturneLight <55106060+NocturneLight@users.noreply.github.com>
2020-03-14 15:08:30 -0500

    Moved the model object to folder.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using ChandlerSpeaks.Models;
-using IronPython.Hosting;
+//using IronPython.Hosting;
 
 namespace ChandlerSpeaks.Controllers
 {
@@ -14,43 +14,44 @@ namespace ChandlerSpeaks.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
+        // Create variables here.
+        //dynamic testVariable;
+
         // Create functions here.
         [HttpPost]
-        public IActionResult Index(FormModel model)
+        public IActionResult Index(FilterModel model)
         {
+            model.DisplayAllValues();
+
             // Create an instance of the Python engine.
-            var pythonEngine = Python.CreateEngine();
+            //var pythonEngine = Python.CreateEngine();
 
             // Create an instance of the scope.
-            var pythonScope = pythonEngine.CreateScope();
+            //var pythonScope = pythonEngine.CreateScope();
 
             // Create an instance of an operation.
-            var operation = pythonEngine.Operations;
+            //var operation = pythonEngine.Operations;
 
             // Compile and execute the Python program.
             //var pythonScript = pythonEngine.CreateScriptSourceFromFile("HelloWorld.py").Compile().Execute(pythonScope);
-            var pythonScript = pythonEngine.ExecuteFile("HelloWorld.py", pythonScope);
+            //var pythonScript = pythonEngine.ExecuteFile("HelloWorld.py", pythonScope);
 
             // Retrieve the Python program's list from the scope instance.
             //IronPython.Runtime.List pythonList = pythonScope.GetVariable("testDiction");
             
             // Retrieve an instance of the object.
-            object foobarTest = pythonScope.GetVariable("fooTest");
+            //object foobarTest = pythonScope.GetVariable("fooTest");
 
             //Func<string> sayHello = pythonEngine.Operations.GetMember<Func<string>>(foobarTest, "f");
-            Func<int> sayAge = pythonEngine.Operations.GetMember<Func<int>>(foobarTest, "getAge");
+            //Func<int> sayAge = pythonEngine.Operations.GetMember<Func<int>>(foobarTest, "getAge");
             //Func<List<string>> Hi = pythonEngine.Operations.GetMember<Func<List<string>>>(foobarTest, "");
 
             //string result = sayHello();
-            int age = sayAge();
+            //int age = sayAge();
 
             //Debug.WriteLine(result);
-            Debug.WriteLine(age);    
-            
+            //Debug.WriteLine(age);    
 
-            
-            
-  
             /*
             foreach (var item in model.CompanyAge)
             {
@@ -61,8 +62,10 @@ namespace ChandlerSpeaks.Controllers
             //return Content("Hi! The size is: ");
             return View("Index");
         }
+
         
 
+        // Prebuilt functions.
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
@@ -84,17 +87,5 @@ namespace ChandlerSpeaks.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
-    }
-
-    public class FormModel
-    {
-        // Create variables for storing each section's choice's truth values.
-        public List<bool> CompanyAge {get; set;}
-        public List<bool> GrantType {get; set;}
-        public List<bool> Location {get; set;}
-        public List<bool> Race {get; set;}
-        public List<bool> ReligiousAffiliation {get; set;}
-        public List<bool> ReligiousIdentification {get; set;}
-        public List<bool> DueDates {get; set;}
     }
 }
